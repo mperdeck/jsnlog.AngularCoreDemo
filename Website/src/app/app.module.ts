3a547e968870dd270eb39a0d65d2375617511b7b
mperdeck <matt.perdeck@readify.net>
2017-05-29 22:39:58 +1000

    add jsnlog service to app module providers

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { JSNLogService } from './jsnlog/jsnlog.service';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,9 @@ import { AppComponent } from './app.component';
   ],
   declarations: [
     AppComponent
+  ],
+  providers: [
+      JSNLogService
   ],
   bootstrap: [ AppComponent ]
 })
