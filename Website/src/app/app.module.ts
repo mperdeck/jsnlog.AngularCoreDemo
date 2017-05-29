3f13ff18d300daefcecb445e62856598163721f8
mperdeck <matt.perdeck@readify.net>
2017-05-30 00:02:35 +1000

    add uncaught exception handler

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { JSNLogService } from './jsnlog/jsnlog.service';
+import { UncaughtExceptionHandler } from './uncaughtExceptionHandler/uncaughtExceptionHandler';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   providers: [
-      JSNLogService
+      JSNLogService,
+      { provide: ErrorHandler, useClass: UncaughtExceptionHandler }
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
@@ -0,0 +1,8 @@
+﻿import { ErrorHandler } from '@angular/core';
+import { JL } from 'jsnlog';
+
+export class UncaughtExceptionHandler implements ErrorHandler {
+    handleError(error: any) {
+        JL().fatalException('Uncaught Exception', error);
+    }
+}
