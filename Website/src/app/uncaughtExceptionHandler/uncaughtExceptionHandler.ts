de63eed948b71e087ea4bcc2dc27c071545ed140
mperdeck <matt.perdeck@readify.net>
2017-05-31 20:44:56 +1000

    attempt to inject jsnlog service into uncaught exception handler

--- Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
+++ Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
@@ -1,8 +1,16 @@
 ﻿import { ErrorHandler } from '@angular/core';
 import { JL } from 'jsnlog';
+import { JSNLogService } from '../jsnlog/jsnlog.service';
 
 export class UncaughtExceptionHandler implements ErrorHandler {
+
+    JL: JL.JSNLog;
+
+    constructor(jsnlogService: JSNLogService) {
+        this.JL = jsnlogService.getJSNLog();
+    }
+
     handleError(error: any) {
-        JL().fatalException('Uncaught Exception', error);
+        this.JL().fatalException('Uncaught Exception', error);
     }
 }
