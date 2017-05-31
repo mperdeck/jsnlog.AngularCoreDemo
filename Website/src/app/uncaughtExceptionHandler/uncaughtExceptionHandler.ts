246553a3cecf48d9a1c81fb99279b2212bc5952b
mperdeck <matt.perdeck@readify.net>
2017-05-31 20:45:21 +1000

    Revert "attempt to inject jsnlog service into uncaught exception handler"
    This reverts commit de63eed948b71e087ea4bcc2dc27c071545ed140.

--- Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
+++ Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
@@ -1,16 +1,8 @@
 ﻿import { ErrorHandler } from '@angular/core';
 import { JL } from 'jsnlog';
-import { JSNLogService } from '../jsnlog/jsnlog.service';
 
 export class UncaughtExceptionHandler implements ErrorHandler {
-
-    JL: JL.JSNLog;
-
-    constructor(jsnlogService: JSNLogService) {
-        this.JL = jsnlogService.getJSNLog();
-    }
-
     handleError(error: any) {
-        this.JL().fatalException('Uncaught Exception', error);
+        JL().fatalException('Uncaught Exception', error);
     }
 }
