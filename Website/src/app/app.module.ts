5450bc781e13e287ed8203c042042c3eddb673b2
mperdeck <matt.perdeck@readify.net>
2017-06-03 20:47:56 +1000

    put uncaught exception handler in app.module to simplify things

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 // Import definitions for the providers list
 import { JSNLogService } from './jsnlog/jsnlog.service';
-import { UncaughtExceptionHandler } from './uncaughtExceptionHandler/uncaughtExceptionHandler';
 
 import { JL } from 'jsnlog';
+import { ErrorHandler } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+class UncaughtExceptionHandler implements ErrorHandler {
+    handleError(error: any) {
+        JL().fatalException('Uncaught Exception', error);
+    }
+}
 
 // Configure the JSNLog logging library.
 // See http://jsnlog.com/Documentation/JSNLogJs
--- Website/src/app/uncaughtExceptionHandler/uncaughtExceptionHandler.ts
+++
@@ -1,8 +0,0 @@
-﻿import { ErrorHandler } from '@angular/core';
-import { JL } from 'jsnlog';
-
-export class UncaughtExceptionHandler implements ErrorHandler {
-    handleError(error: any) {
-        JL().fatalException('Uncaught Exception', error);
-    }
-}
