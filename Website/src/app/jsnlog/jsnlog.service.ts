09aa9e7917b8d0e1d7597cea97af3d0cd8c59200
mperdeck <matt.perdeck@readify.net>
2017-06-04 14:31:30 +1000

    improve documentation for getJSNLog method

--- Website/src/app/jsnlog/jsnlog.service.ts
+++ Website/src/app/jsnlog/jsnlog.service.ts
@@ -3,5 +3,8 @@ import { JL } from 'jsnlog';
 
 @Injectable()
 export class JSNLogService {
+    // Returns a reference to the JSNLog object.
+    // JL.JSNLog is the interface that is implemented by the JSNLog object.
+    // To mock JSNLog, create a mock object that implements this same interface.
     getJSNLog(): JL.JSNLog { return JL; }
 }
